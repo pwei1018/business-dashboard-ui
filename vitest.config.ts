c22ebf9b914797551d3abba709a5144165c4c5cf
pwei1018 <patrick.wei@gov.bc.ca>
2025-03-21 11:41:57 -0700

    test

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,18 @@
 import { fileURLToPath } from 'node:url'
+import path from 'path'
 import { defineVitestConfig } from '@nuxt/test-utils/config'
 
 export default defineVitestConfig({
   test: {
     dir: 'tests',
+    coverage: {
+      provider: 'v8',
+      reportsDirectory: path.resolve(__dirname, 'tests/coverage'), // This ensures an absolute path,
+      reporter: ['text', 'clover', 'html'],
+      include: [
+        'src/**'
+      ]
+    },
     environment: 'nuxt',
     environmentOptions: {
       nuxt: {
