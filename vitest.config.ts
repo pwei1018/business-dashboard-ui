d8f4d0e7de2f1458a3a65158ec968650f04d1fd0
Patrick Wei <44277752+pwei1018@users.noreply.github.com>
2025-03-21 16:45:36 -0700

    test (#1)
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test1
    
    * test3
    
    * test
    
    * tedt

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,25 @@
 import { fileURLToPath } from 'node:url'
+import path from 'path'
 import { defineVitestConfig } from '@nuxt/test-utils/config'
 
 export default defineVitestConfig({
   test: {
     dir: 'tests',
+    coverage: {
+      provider: 'v8',
+      ignoreEmptyLines: true,
+      reportsDirectory: path.resolve(__dirname, 'tests/coverage'), // This ensures an absolute path,
+      // you can include other reporters, but 'json-summary' is required, json is recommended
+      reporter: ['text', 'json-summary', 'json'],
+      // If you want a coverage reports even if your tests are failing, include the reportOnFailure option
+      reportOnFailure: true,
+      thresholds: {
+        lines: 35,
+        branches: 40,
+        functions: 5,
+        statements: 30
+      }
+    },
     environment: 'nuxt',
     environmentOptions: {
       nuxt: {
