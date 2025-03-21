d2801a8cf19afe65ef206a405744ed0022fc53cc
pwei1018 <patrick.wei@gov.bc.ca>
2025-03-21 15:14:56 -0700

    test

--- vitest.config.ts
+++ vitest.config.ts
@@ -11,7 +11,13 @@ export default defineVitestConfig({
       // you can include other reporters, but 'json-summary' is required, json is recommended
       reporter: ['text', 'json-summary', 'json'],
       // If you want a coverage reports even if your tests are failing, include the reportOnFailure option
-      reportOnFailure: true
+      reportOnFailure: true,
+      thresholds: {
+        lines: 80,
+        branches: 80,
+        functions: 80,
+        statements: 80
+      }
     },
     environment: 'nuxt',
     environmentOptions: {
