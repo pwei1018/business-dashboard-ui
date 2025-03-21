8f2d23c67a01aad7300a7b46c41574dbfede077b
pwei1018 <patrick.wei@gov.bc.ca>
2025-03-21 14:58:16 -0700

    test

--- vitest.config.ts
+++ vitest.config.ts
@@ -8,7 +8,10 @@ export default defineVitestConfig({
     coverage: {
       provider: 'v8',
       reportsDirectory: path.resolve(__dirname, 'tests/coverage'), // This ensures an absolute path,
-      reporter: ['text', 'clover', 'html']
+      // you can include other reporters, but 'json-summary' is required, json is recommended
+      reporter: ['text', 'json-summary', 'json'],
+      // If you want a coverage reports even if your tests are failing, include the reportOnFailure option
+      reportOnFailure: true
     },
     environment: 'nuxt',
     environmentOptions: {
