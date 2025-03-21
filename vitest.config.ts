8e6259a899e8936b95839989a3eb33cc94234b43
pwei1018 <patrick.wei@gov.bc.ca>
2025-03-21 15:23:30 -0700

    test1

--- vitest.config.ts
+++ vitest.config.ts
@@ -11,7 +11,7 @@ export default defineVitestConfig({
       // you can include other reporters, but 'json-summary' is required, json is recommended
       reporter: ['text', 'json-summary', 'json'],
       // If you want a coverage reports even if your tests are failing, include the reportOnFailure option
-      reportOnFailure: true,
+      reportOnFailure: false,
       thresholds: {
         lines: 80,
         branches: 80,
